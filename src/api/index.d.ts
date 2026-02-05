72b2aaa77a0b7b8ea982ee402f9720a38fa8a511
rlongre <rlongre@gmail.com>
2026-02-05 07:46:12 -0500

    Clean up build artifacts

--- src/api/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function makeApiRequest<T = any>(method: string, path: string, data?: any, timeout?: number): Promise<T>;
-//# sourceMappingURL=index.d.ts.map
--- src/commands/space.d.ts
+++
@@ -1,6 +0,0 @@
-import { Command } from 'commander';
-export declare const createSpaceCommand: Command;
-export declare const createFolderCommand: Command;
-export declare const createListCommand: Command;
-export declare const listTeamTasksCommand: Command;
-//# sourceMappingURL=space.d.ts.map
